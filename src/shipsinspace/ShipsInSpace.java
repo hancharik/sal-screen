818ce2651272918eebf94440916b24c5e10d1772
hancharik <mah537@psu.edu>
2016-01-16 18:06:48 -0500

    this is working so smooth, we should fork any future work. this is the best version so far

--- src/shipsinspace/ShipsInSpace.java
+++ src/shipsinspace/ShipsInSpace.java
@@ -10,7 +10,7 @@ public class ShipsInSpace {
 
     
     
-    public static int size = 42 ;
+    public static int size = 21 ;
     public static Thing[][] spaceMap = new Thing [size][size];
     public static GalaxyMap gtest = new GalaxyMap();
     public static PlayerShip playerShip = new PlayerShip();
